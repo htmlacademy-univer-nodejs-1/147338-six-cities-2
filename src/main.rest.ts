e606a38b93d7dd934c8d356132b3e0188abeb2d5
ella <ellasidorova@yandex.ru>
2023-12-11 14:39:18 +0300

    Добавлена привязка для DatabaseClient

--- src/main.rest.ts
+++ src/main.rest.ts
@@ -4,6 +4,7 @@ import { RestApplication } from './rest/index.js';
 import { Logger, PinoLogger } from './shared/libs/logger/index.js';
 import { RestConfig, Config, RestSchema } from './shared/libs/config/index.js';
 import { Components } from './shared/types/index.js';
+import { DatabaseClient, MongoDatabaseClient } from './shared/libs/database-client/index.js';
 
 async function bootstrap(): Promise<void> {
   const container = new Container();
@@ -11,6 +12,7 @@ async function bootstrap(): Promise<void> {
   container.bind<RestApplication>(Components.RestApplication).to(RestApplication).inSingletonScope();
   container.bind<Logger>(Components.Logger).to(PinoLogger).inSingletonScope();
   container.bind<Config<RestSchema>>(Components.Config).to(RestConfig).inSingletonScope();
+  container.bind<DatabaseClient>(Components.DatabaseClient).to(MongoDatabaseClient).inSingletonScope();
 
   const application = container.get<RestApplication>(Components.RestApplication);
   await application.init();
--- src/shared/types/components.enum.ts
+++ src/shared/types/components.enum.ts
@@ -2,4 +2,5 @@ export const Components = {
   RestApplication: Symbol.for('RestApplication'),
   Logger: Symbol.for('Logger'),
   Config: Symbol.for('Config'),
+  DatabaseClient: Symbol.for('DatabaseClient'),
 } as const;
