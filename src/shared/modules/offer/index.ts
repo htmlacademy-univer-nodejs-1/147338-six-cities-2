17590834e6a943a5440387c606dec5e9d5f64a4f
ella <ellasidorova@yandex.ru>
2023-12-24 01:06:21 +0300

    offer modified

--- src/shared/modules/offer/index.ts
+++ src/shared/modules/offer/index.ts
@@ -3,3 +3,5 @@ export * from './offer-service.interface.js';
 export * from './offer.container.js';
 export * from './offer.entity.js';
 export * from './dto/create-offer.dto.js';
+export * from './dto/update-offer.dto.js';
+
